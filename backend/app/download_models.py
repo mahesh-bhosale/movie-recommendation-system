d8cd5157d32e3ee18bd236344cee67a073bc6e96
Mahesh Bhosale <maheshbhosale1212004@gmail.com>
2025-04-30 23:45:10 +0530

    update download model code

--- backend/app/download_models.py
+++ backend/app/download_models.py
@@ -121,7 +121,7 @@ def download_from_drive(file_id, output, retries=3, min_size=None):
             if file_size > 50 * 1024 * 1024:  # > 50MB
                 wait_time = min(30, int(file_size / (10 * 1024 * 1024)))  # 1s per 10MB, max 30s
                 print(f"Waiting {wait_time} seconds for file to be fully written...")
-                time.sleep(wait_time)       
+                time.sleep(2*wait_time)       
 
             # if file_size > 50 * 1024 * 1024:  # If file is larger than 50MB
             #     print("Waiting for file to be fully written...")
